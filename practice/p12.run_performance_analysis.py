9dbe0116df666f7a704e3694bdcf378ac13bdef5
우광택 <yerihan@gmail.com>
2026-08-13 19:11:04 +0900

    P12-2

--- practice/p12.run_performance_analysis.py
+++ practice/p12.run_performance_analysis.py
@@ -2,4 +2,4 @@ def run_performance_analysis(sizes, pattern_dict, filters_dict):
 
     for n in sizes:
         avg_time = measure_mac_time(pattern_dict[n],filters_dict[n])
-        print(f"크기: {n} / 평균시간(ms): {avg_time} / 연산횟수: {nxn}") 
+        print(f"크기: {n} / 평균시간(ms): {avg_time} / 연산횟수: {n*n}") 
