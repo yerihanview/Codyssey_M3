a529cff5570166f448915376480cdef7862c4a9a
우광택 <yerihan@gmail.com>
2026-08-13 17:56:49 +0900

    P7-1

--- practice/p7.normalize_label.py
+++ practice/p7.normalize_label.py
@@ -1,10 +1,9 @@
 LABEL_MAP = {
     "cross": "Cross",
     "+": "Cross",
-    "x": "X",
-    "X": "X"
+    "x": "X" 
 }
 
 def normalize_label(raw_label):
     
-    return LABEL_MAP[raw_label.lower()].get()
+    return LABEL_MAP.get(raw_label.lower())
