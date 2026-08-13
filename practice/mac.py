559298f9d2ca6494548fd3e2232389698a9ae60b
우광택 <yerihan@gmail.com>
2026-08-13 14:15:33 +0900

    p1

---
+++ practice/mac.py
@@ -0,0 +1,10 @@
+def mac(pattern, filter):
+
+    n = len(pattern)
+    score = 0
+
+    for i in range(n):
+        for j in range(n):
+            score += pattern[i][j] * filter[i][j]
+
+    return score
