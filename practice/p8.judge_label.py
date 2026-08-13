58bdd2f14e3bd607fff6f7471671328f03b64061
우광택 <yerihan@gmail.com>
2026-08-13 18:04:13 +0900

    P8-1

--- practice/p8.judge_label.py
+++ practice/p8.judge_label.py
@@ -2,12 +2,11 @@ EPSILON = 1.e-9
 
 def judge_label(score_cross, score_x):
 
-    diff = abs(score_cross, score_x)
+    decision = judge(score_cross, score_x)
 
-    if diff < EPSILON:
-        return "UNDEFINED"
-
-    if score_cross > score_x:
+    if decision == "UNDECIDED":
+        return decision
+    elif decision == "A":
         return "Cross"
     else:
         return "X"
